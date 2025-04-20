e92404c37e24bb40bc4eb2b115fa681e0138aef4
Atos Singh <sheemaratos@gmail.com>
2025-04-20 18:23:42 +0200

    prova programma

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include<iostream>
+#include<string>
 using namespace std;
+
+bool gitCheck();
 
 int main(int argc, char *argv[]) {
     
+    if(!gitCheck()){
+        cout<<"git non è installato!"<<endl;
+        return -1;
+    }
+
     if(argv[1] == nullptr) {
         cout<<"Nessun messaggio di commit fornito."<<endl;
-        return -1;
+        return -2;
     }else if(argv[2] == nullptr) {
         cout<<"Nessun branch fornito."<<endl;
-        return -1;
+        return -3;
     }
     string messaggio = argv[1];
     string branch = argv[2];
@@ -18,7 +26,7 @@ int main(int argc, char *argv[]) {
     string comando = "git commit -m \""+messaggio+"\"";
     string comando2 = "git push origin \""+branch+"\"";
     
-    cout<<"aggiungendo i file git..."<<endl;
+    cout<<"aggiungendo i file..."<<endl;
     system("git add .");
 
     cout<<"eseguendo il commit..."<<endl;
@@ -31,4 +39,19 @@ int main(int argc, char *argv[]) {
 
 
     return 0;
+}
+
+
+bool gitCheck(){
+    cout<<"Controllo se git è installato..."<<endl;
+    string ris;
+    #ifdef _WIN32
+        ris = "NUL 2>&1";
+    #else
+        ris = "/dev/null 2>&1";
+    #endif
+
+    string command = "git --version > " + ris;
+    int result = system(command.c_str());
+    return result == 0;
 }
