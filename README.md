249b03576621cd3702c14339b3e9328bdfd844ff
Atos Singh <sheemaratos@gmail.com>
2025-04-20 21:14:06 +0200

    English translation

--- README.md
+++ README.md
@@ -2,15 +2,19 @@
 
 programma che ti permette di automatizzare il commit e push per la propria repository
 
-come funziona:
+program that automates the commiting and pushing of your directory
+
+how it works
 
 ```sh
-programma.exe "Messaggio commit" "Branch"
+AutoGit "Commit message" "Branch"
 ```
 
-### da dove scaricarlo
+### where to download
 
 nella repository vai in Actions, scegli l'ultimo workflow e in Artifacts troverai le versioni per ubuntu e windows
+
+in the repository go in Actions, select the latest workflow and in Artifacts you will find the builds for Linux and Windows
 
 ## License
 
--- main.cpp
+++ main.cpp
@@ -7,32 +7,32 @@ bool gitCheck();
 int main(int argc, char *argv[]) {
     
     if(!gitCheck()){
-        cout<<"git non è installato!"<<endl;
+        cout<<"git is not installed!"<<endl;
         return -1;
     }
 
     if(argv[1] == nullptr) {
-        cout<<"Nessun messaggio di commit fornito."<<endl;
+        cout<<"No comment for commit inputed."<<endl;
         return -2;
     }else if(argv[2] == nullptr) {
-        cout<<"Nessun branch fornito."<<endl;
+        cout<<"No branch inputed."<<endl;
         return -3;
     }
     string messaggio = argv[1];
     string branch = argv[2];
 
-    cout<<"messaggio: "<<messaggio<<endl;
-    cout<<"branch: "<<branch<<endl;
+    cout<<"Message: "<<messaggio<<endl;
+    cout<<"Branch: "<<branch<<endl;
     string comando = "git commit -m \""+messaggio+"\"";
     string comando2 = "git push origin \""+branch+"\"";
     
-    cout<<"aggiungendo i file..."<<endl;
+    cout<<"Adding the files..."<<endl;
     system("git add .");
 
-    cout<<"eseguendo il commit..."<<endl;
+    cout<<"commiting..."<<endl;
     system(comando.c_str());
 
-    cout<<"mandando al branch "<<branch<<endl;
+    cout<<"pushing to "<<branch<<endl;
     system(comando2.c_str());
     
 
@@ -42,7 +42,7 @@ int main(int argc, char *argv[]) {
 
 
 bool gitCheck(){
-    cout<<"Controllo se git è installato..."<<endl;
+    cout<<"Checking if git is installed..."<<endl;
     string ris;
     #ifdef _WIN32
         ris = "NUL 2>&1";
